56ca8c3f02e32149209b3f676c496f016f981765
Brian <brian.giupponi@infinixsoft.com>
2018-05-14 18:07:53 -0300

    Working on My Message Cell

--- Example/ISChat/ViewController.swift
+++ Example/ISChat/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import ISChat
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
