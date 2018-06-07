6172739829fe51577096305598f5c765e08de09d
Brian <brian.giupponi@infinixsoft.com>
2018-06-07 17:57:36 -0300

    Detalles del chat / Tiene que subir el textField con el teclado

--- ISChat/Classes/Application/Conversation Detail/ConversationDetailViewController.swift
+++ ISChat/Classes/Application/Conversation Detail/ConversationDetailViewController.swift
@@ -24,13 +24,60 @@ class ConversationDetailViewController: UIViewController {
         super.viewDidLoad()
         
         let cellBundle = Bundle(for: ConversationDetailViewController.self)
-        let nib = UINib(nibName: "MyMessageTableViewCell", bundle: cellBundle)
-        self.tableView.register(nib, forCellReuseIdentifier: "MyMessageTableViewCell")
+        let nibMM = UINib(nibName: "MyMessageTableViewCell", bundle: cellBundle)
+        let nibOM = UINib(nibName: "OtherMessageTableViewCell", bundle: cellBundle)
+        self.tableView.register(nibMM, forCellReuseIdentifier: "MyMessageTableViewCell")
+        self.tableView.register(nibOM, forCellReuseIdentifier: "OtherMessageTableViewCell")
 
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(_keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(_keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc private func _keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {return}
+        guard let _duration = userInfo[UIKeyboardAnimationDurationUserInfoKey]else { return }
+        guard let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey] else { return }
+        guard let keyboardFrameEnd = userInfo[UIKeyboardFrameEndUserInfoKey] else { return }
+        
+        let curveOption = UIViewAnimationOptions(rawValue: UInt((curve as AnyObject).integerValue << 16))
+        let keyboardFrameEndRectFromView = view.convert((keyboardFrameEnd as AnyObject).cgRectValue, from: nil)
+        let duration = (_duration as AnyObject).doubleValue ?? 1.0
+        let viewAnimationOptions: UIViewAnimationOptions = [curveOption, .beginFromCurrentState]
+        
+        keyboardWillShow(endRect: keyboardFrameEndRectFromView, duration: duration, animationOptions: viewAnimationOptions)
+    }
+    
+    @objc private func _keyboardWillHide(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {return}
+        guard let _duration = userInfo[UIKeyboardAnimationDurationUserInfoKey]else { return }
+        guard let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey] else { return }
+        
+        let curveOption = UIViewAnimationOptions(rawValue: UInt((curve as AnyObject).integerValue << 16))
+        let duration = (_duration as AnyObject).doubleValue ?? 1.0
+        let viewAnimationOptions: UIViewAnimationOptions = [curveOption, .beginFromCurrentState]
+        
+        keyboardWillHide(duration: duration, animationOptions: viewAnimationOptions)
+    }
+    
+    func keyboardWillShow(endRect: CGRect, duration: Double, animationOptions: UIViewAnimationOptions) {}
+    func keyboardWillHide(duration: Double, animationOptions: UIViewAnimationOptions) {}
 
 }
 
@@ -45,7 +92,11 @@ extension ConversationDetailViewController: UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "MyMessageTableViewCell") as! MyMessageTableViewCell
+        if indexPath.row % 2 == 0 {
+            return tableView.dequeueReusableCell(withIdentifier: "MyMessageTableViewCell") as! MyMessageTableViewCell
+        }else {
+            return tableView.dequeueReusableCell(withIdentifier: "OtherMessageTableViewCell") as! OtherMessageTableViewCell
+        }
     }
     
 }
